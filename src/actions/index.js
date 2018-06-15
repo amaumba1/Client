85bbb771e2ae00cb97518a6a2f01259e3c481d9f
Ally Maumba <amaumba1@gmail.com>
2018-06-15 04:00:36 -0500

    modify authReducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,8 +2,10 @@ import axios from 'axios'
 import { AUTH_USER } from './types'
 
 
-export const signup = (formProps) => dispatch => {
-    axios.post('http://localhost:5000/api/users/register', formProps)
+export const signup = (formProps) => async dispatch => {
+    const response = await axios.post('http://localhost:5000/api/users/register', formProps)
+
+    dispatch({ type: AUTH_USER , payload: response.data.token })
 
 }
 
--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,8 +1,15 @@
+import { AUTH_USER } from '../actions/types'
+
 const INTIAL_STATE = {
     authenticated: '',
     errorMessage: ''
 };
 
 export default(state = INTIAL_STATE, action) => { 
-    return state; 
+    switch(action.type) {
+        case AUTH_USER:
+            return { ...state, authenicated: action.payload }
+        default:
+            return state; 
+    }
 }
